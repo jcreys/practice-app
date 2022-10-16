a80922c67c34d2234c1686d16d28d57320d68fdf
Joshua Reyes <reyes.j15@gmail.com>
2022-10-16 13:23:24 -0400

    added models

--- db/index.js
+++ db/index.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/practice-app-db') //environment or development
 
+const User = sequelize.define('user',
+{
+    firstName: Sequelize.STRING,
+})
+
+const Task = sequelize.define('task', {
+    name: Sequelize.STRING
+})
 module.exports={
     sequelize
 }
+
--- server.js
+++ server.js
@@ -10,9 +10,39 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get('/task', async(req,res,next)=>{
+    try{
+        const task = await Task.findAll();
+        res.send(task);
+    }catch(ex){
+        next(ex)
+    }
+
+})
+
+app.get('/user', (req,res,next)=>{
+    try{
+        const user = await User.findAll();
+        res.send(user);
+    }catch(ex){
+        next(ex)
+    }
+
+})
+
+
 const init = async () => {
   console.log("calling init");
   await sequelize.sync({ force: true });
+  await Promise.all([
+    User.create({ firstName: 'Joshua' }),
+    User.create({ firstName: 'Lucy' }),
+    User.create({ firstName: 'Ethyl' }),
+    Task.create({ name: 'Buy juice' }),
+    Task.create({ name: 'Go to gym' }),
+    Task.create({ name: 'Get that bread' }),
+    Task.create({ name: 'Code more' }),
+])
 };
 
 init();
