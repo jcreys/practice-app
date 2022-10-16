4d352151fed8d694e6ee1721eb6af64f2f7c3855
Joshua Reyes <reyes.j15@gmail.com>
2022-10-16 12:47:35 -0400

    added get route to send index html file

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path')
 app.listen(port, console.log(`listening on port ${port}`))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
